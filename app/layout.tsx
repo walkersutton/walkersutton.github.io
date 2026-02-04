0505dc5301bd09285c9a43e4054938676a4e22e7
Walker Sutton <walker.c.sutton@gmail.com>
2026-02-04 01:08:55 -0800

    new shi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Script from "next/script";
+import Image from "next/image";
 import { hanken, lexend } from "./fonts";
 import "./globals.css";
 import Link from "next/link";
@@ -43,7 +44,16 @@ export default function RootLayout({
         className={`bg-[var(--color-bg)] text-[var(--color-text)] antialiased mx-auto max-w-[650px] overflow-x-hidden px-6 py-12 md:overflow-x-visible md:py-16`}
       >
         <header className="mb-15">
-          <h1 className={`${lexend.className} text-xl mb-2`}>Walker Sutton</h1>
+          <div className="flex items-center gap-2 mb-2">
+            <Image
+              src="/images/white_logo.png"
+              alt="Logo"
+              width={25}
+              height={25}
+              className="dark:invert-0 invert grayscale -ml-1"
+            />
+            <h1 className={`${lexend.className} text-xl`}>Walker Sutton</h1>
+          </div>
           <Socials socials={socials} />
         </header>
         {children}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const config: NextConfig = {
   output: "export",
+  images: { unoptimized: true },
 };
 
 export default config;
